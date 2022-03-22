fbd9b7eb6095ee4e796284642e1413771c242d41
LukeSlater02 <lukeslater101@gmail.com>
2022-03-22 10:12:38 -0500

    cleaned up the main.js, slight mod to the html and created the filter-by-state for the parks

--- scripts/parks/parkDropdown.js
+++ scripts/parks/parkDropdown.js
@@ -1,32 +1,20 @@
-import {getParks} from "./ParkDataManager.js"
+import { getParks } from "./ParkDataManager.js"
 
-// document.querySelector(".dropdown-menus").addEventListener("change", event => {
-//     const filteredParks = []
-//     if (event.target.id === "state-dropdown"){
-//         getParks().then(data => {
-//             filteredParks = data.filter(singlePark => {
-//                 if (singlePark.states.includes(event.target.value)){
-//                     return singlePark
-//                 }
-//             })
-//             for (const park of filteredParks){
-//                 parksDropdownHTML(park)
-//             }
-//         })
-//         console.log(filteredPars);
-//     }
-// })
 
-const parksDropdownHTML = (park) => {
-    document.querySelector("#filteredParks").insertAdjacentHTML("beforeend", `<option value="${park.parkCode}">${park.fullName}</option>`)
+const parksDropdownHTML = (parksData) => {
+    document.querySelector(".parksBox").innerHTML = `<select name="filteredParks" id="filteredParks">
+    <option value="">-- Select a Park --</option>
+  </select>`
+
+    for (const park of parksData.data) {
+        document.querySelector("#filteredParks").insertAdjacentHTML("beforeend", `<option value="${park.parkCode}">${park.fullName}</option>`)
+    }
 }
 
-export const insertParks = () => {
-    getParks()
-.then(parksData => {
-    for (const park of parksData.data) {
-        parksDropdownHTML(park)
-    }
-})
+export const insertParks = (link) => {
+    getParks(link)
+        .then(parksData => {
+            parksDropdownHTML(parksData)
+        })
 }
 
