5561dbc399b7116c08c4e99de7360f511708fd41
acheatham87 <acheatham87@gmail.com>
2022-03-23 10:15:21 -0500

    preview aside bar fully functional, still needs styling

--- scripts/parks/parksPreview.js
+++ scripts/parks/parksPreview.js
@@ -1,21 +1,15 @@
-// import { getParks } from "./ParkDataManager.js";
 
 const parksPreviewHTML = (parks) => {
     return `
     <section>
         <header>
-            <h2 class=>${parks.fullName}</h2>
+            <h3>${parks}</h3>
         </header>
-        <p>${parks.addresses.city}, ${parks.addresses.stateCode}</p>
     </section>`
 }
 
-// export const previewParks = (index) => {
-//     let selectedPark = {}
-//     const parkElement = document.querySelector(".parkPreview");
-//     getParks().then((data) => {
-//         selectedPark = data[index - 1]
-//         parkElement.innerHTML = parksPreviewHTML(selectedPark)
-//     })
-// } 
+export const previewPark = (value) => {
+    const parkElement = document.querySelector(".parkPreview");
+    parkElement.innerHTML = parksPreviewHTML(value)
+}
 
